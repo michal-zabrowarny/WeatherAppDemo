3d85af1bb9031ea02faebd0419b999b444c31a6b
michal-zabrowarny <m.zabrowarny@gmail.com>
2025-06-20 18:39:41 +0200

    Delete weather-app-demo1.0 directory

--- weather-app-demo1.0/src/main/java/com/michalz/weather_app_demo/exception/ExternalApiException.java
+++
@@ -1,7 +0,0 @@
-package com.michalz.weather_app_demo.exception;
-
-public class ExternalApiException extends RuntimeException {
-    public ExternalApiException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
