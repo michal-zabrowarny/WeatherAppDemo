0f287974b139125e062045670cd1e10a76a34b18
michal-zabrowarny <m.zabrowarny@gmail.com>
2025-06-29 17:13:33 +0200

    Add files via upload

--- src/main/java/com/michalz/weather_app_demo/config/ApplicationConfig.java
+++ src/main/java/com/michalz/weather_app_demo/config/ApplicationConfig.java
@@ -20,7 +20,7 @@ public class ApplicationConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8081")
+                        .allowedOrigins("http://localhost:8081, https://weatherappdemo-596h.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
